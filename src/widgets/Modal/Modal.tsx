8f68aa38043836d071a6c1854561860aa6e000cf
elisalech <elis.alech900@gmail.com>
2021-12-11 12:45:06 +0300

    feat: close modal on esc

--- src/widgets/Modal/Modal.tsx
+++ src/widgets/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Button } from 'components/Button/Button';
 import Headline from 'components/Headline';
@@ -14,6 +14,7 @@ import { Card } from 'components/Card/Card';
 
 interface Props extends InjectedProps {
   title: string;
+  closeOnEscape?: boolean;
   showCloseButton?: boolean;
   className?: string;
   onBack?: () => void;
@@ -25,29 +26,48 @@ const Modal: React.FC<Props> = ({
   onBack,
   children,
   className,
+  closeOnEscape = true,
   showCloseButton = true,
-}) => (
-  <Card withPadding={false} className={classnames(styles.base, className)}>
-    <Row className={styles.header}>
-      <div>
-        {onBack && (
-          <Button variant="text" onClick={onBack} area-label="go back">
-            <IArrowLeft color="primary" />
+}) => {
+  useEffect(() => {
+    if (closeOnEscape) {
+      const handlePress = (ev: KeyboardEvent) => {
+        if (ev.key === 'Escape') {
+          onDismiss?.();
+        }
+      };
+
+      document.addEventListener('keyup', handlePress);
+
+      return () => {
+        document.removeEventListener('keyup', handlePress);
+      };
+    }
+  }, [closeOnEscape, onDismiss]);
+
+  return (
+    <Card withPadding={false} className={classnames(styles.base, className)}>
+      <Row className={styles.header}>
+        <div>
+          {onBack && (
+            <Button variant="text" onClick={onBack} area-label="go back">
+              <IArrowLeft color="primary" />
+            </Button>
+          )}
+          <Headline>{title}</Headline>
+        </div>
+        {showCloseButton && (
+          <Button
+            variant="text"
+            onClick={onDismiss}
+            aria-label="Close the dialog">
+            <IClose />
           </Button>
         )}
-        <Headline>{title}</Headline>
-      </div>
-      {showCloseButton && (
-        <Button
-          variant="text"
-          onClick={onDismiss}
-          aria-label="Close the dialog">
-          <IClose />
-        </Button>
-      )}
-    </Row>
-    <div>{children}</div>
-  </Card>
-);
+      </Row>
+      <div>{children}</div>
+    </Card>
+  );
+};
 
 export default Modal;
