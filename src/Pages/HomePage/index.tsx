9457d5e93cddfeff92171e63ffa29cbac06550c2
elisalech <elis.alech900@gmail.com>
2022-01-10 01:19:30 +0300

    feat: upd home page layout

--- src/Pages/HomePage/index.tsx
+++ src/Pages/HomePage/index.tsx
@@ -6,11 +6,20 @@ import styles from './home.module.css';
 export default function HomePage() {
   return (
     <main className={styles.container}>
-      <Headline>TON Decentralized Exchange</Headline>
-      <Text color="gray">
-        Welcome to the TON Decentralized Exchange. Trade and earn with TON
-        ecosistem
-      </Text>
+      <div className={styles.responsive_row}>
+        <div>
+          <Headline className={styles.headline} size="big">
+            TON Decentralized Exchange
+          </Headline>
+          <Text color="gray">
+            Welcome to the TON Decentralized Exchange. Trade and earn with TON
+            ecosistem
+          </Text>
+        </div>
+        <div className={styles.img_wrap}>
+          <img src="/images/page-diamond.svg" />
+        </div>
+      </div>
     </main>
   );
 }
